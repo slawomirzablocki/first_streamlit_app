365d943686c8619e9d250d6c715658dc916f4991
MellyMel Melnyk <121654903+slawomirzablocki@users.noreply.github.com>
2022-12-30 06:23:19 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit
 streamlit.title('My Parents New Healty Diner')
 
 
-streamlit.header('Brekfas Menu')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Outmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothe')
+streamlit.text('Hard Boiled Free Range Egg')
